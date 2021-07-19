a664436efee4a9333e8f73ad63e67df50893191f
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-07-19 12:01:23 -0400

    Initial Commit

---
+++ contracts/Fallback.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.0;
+
+contract Overflow {
+    uint private sellerBalance=0;
+    
+    function add(uint value) public returns (bool, uint){
+        // require(value + sellerBalance >= sellerBalance);
+        sellerBalance += value; // complicated math with possible overflow
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.0"
+      },
+    ]
+  }
+};
