cc6892860caa4cd656f96684fbcf5190f655578e
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-07-19 12:42:08 -0400

    Adding an issue to the contract

--- contracts/Fallback.sol
+++ contracts/Fallback.sol
@@ -5,7 +5,7 @@ contract Overflow {
     uint private sellerBalance=0;
     
     function add(uint value) public returns (bool, uint){
-        require(value + sellerBalance >= sellerBalance);
+        // require(value + sellerBalance >= sellerBalance);
         sellerBalance += value; // complicated math with possible overflow
     }
 }
